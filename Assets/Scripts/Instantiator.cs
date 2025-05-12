d839271864e2c56fdd3a31f015d618c49240cb58
TobiasGerman <tobigerman2019@gmail.com>
2025-05-12 10:16:14 -0300

    Funcion Boton

---
+++ Assets/Scripts/Instantiator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiator : MonoBehaviour
+{
+    public GameObject cubePrefab; // referenciando
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ClonarCubo(); // llamando la funcion 
+    }
+
+    public void ClonarCubo() // creando funcion
+    {
+        Instantiate(cubePrefab);
+    }
+}
