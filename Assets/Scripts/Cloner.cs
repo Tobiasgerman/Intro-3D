6128c8861433ab23444880d2442f469e0f6949e6
TobiasGerman <tobigerman2019@gmail.com>
2025-05-15 09:54:34 -0300

    Scene creada

---
+++ Assets/Scripts/Cloner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloner : MonoBehaviour
+{  
+ public GameObject cubePrefab; // referenciando
+
+// Start is called before the first frame update
+void Start()
+{
+    ClonarCubo(); // llamando la funcion 
+}
+
+public void ClonarCubo() // creando funcion
+{
+    Instantiate(cubePrefab);
+}
+}
+
+
