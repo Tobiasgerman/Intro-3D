986811792ba3d91d921333099b7085420d952453
TobiasGerman <tobigerman2019@gmail.com>
2025-05-22 10:44:49 -0300

    barril desaparece

--- Assets/Scripts/deteccionDeColisiones.cs
+++ Assets/Scripts/deteccionDeColisiones.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class deteccionDeColisiones : MonoBehaviour
 {
-    private void OnCollisionEnter() // funcion para que suceda algo cuando suceda una colision
+    private void OnCollisionEnter(Collision col) // funcion para que suceda algo cuando suceda una colision
     {
         Debug.Log("Contacto");
-        Destroy(gameObject); // destroy hace que desaparezca
+
+        if (col.gameObject.name == "Player")
+        {
+            Destroy(gameObject);
+        }
     }
 }
