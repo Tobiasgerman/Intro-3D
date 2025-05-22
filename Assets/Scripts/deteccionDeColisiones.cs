d2dec8641ea78339c45c74b6f7e89b31fd8c06a6
TobiasGerman <tobigerman2019@gmail.com>
2025-05-22 10:26:14 -0300

    colision barril caja terminada

---
+++ Assets/Scripts/deteccionDeColisiones.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deteccionDeColisiones : MonoBehaviour
+{
+    private void OnCollisionEnter() // funcion para que suceda algo cuando suceda una colision
+    {
+        Debug.Log("Contacto");
+        Destroy(gameObject); // destroy hace que desaparezca
+    }
+}
